2d5847e25ef386678317098e805d74e08f5b50c4
TNTzx <azelynnrituapersonality@gmail.com>
2024-10-24 16:20:48 +0800

    add onserverstart

---
+++ src/main/java/me/tntz/ntfyNotifier/events/EventRegisterer.java
@@ -0,0 +1,19 @@
+package me.tntz.ntfyNotifier.events;
+
+import me.tntz.ntfyNotifier.NtfyNotifier;
+import me.tntz.ntfyNotifier.ntfy.Connector;
+import me.tntz.ntfyNotifier.ntfy.MessageSettings;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+
+public class EventRegisterer {
+    public static void eventSendMessage(String title, String description, MessageSettings messageSettings) {
+        try {
+            Connector.sendMessage(title, description, messageSettings);
+        } catch (IOException | URISyntaxException | InterruptedException e) {
+            NtfyNotifier.LOGGER.error("Unexpected error while trying to send message with title \"{}\": {}", title, e);
+        }
+    }
+}
