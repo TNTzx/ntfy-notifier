2f8a3f9faed945a2b59f6a8cc67b97eb00a087da
TNTzx <azelynnrituapersonality@gmail.com>
2024-10-24 13:04:02 +0800

    add mod id and logger

--- src/main/java/me/tntz/ntfyNotifier/NtfyNotifier.java
+++ src/main/java/me/tntz/ntfyNotifier/NtfyNotifier.java
@@ -1,8 +1,14 @@
 package me.tntz.ntfyNotifier;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NtfyNotifier implements ModInitializer {
+
+    public static final String MOD_ID = "ntfy-notifier";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
