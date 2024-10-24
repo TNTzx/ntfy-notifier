642b5f064860c8a35e44d7c1b9fc2a13c72cf05f
TNTzx <azelynnrituapersonality@gmail.com>
2024-10-24 16:22:28 +0800

    add priority searching

--- src/main/java/me/tntz/ntfyNotifier/ntfy/Priority.java
+++ src/main/java/me/tntz/ntfyNotifier/ntfy/Priority.java
@@ -1,5 +1,12 @@
 package me.tntz.ntfyNotifier.ntfy;
 
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Optional;
+
 public enum Priority {
     MAX("max", 5),
     HIGH("high", 4),
@@ -12,5 +19,12 @@ public enum Priority {
     Priority(String postName, int id) {
         this.POST_NAME = postName;
         this.ID = id;
+    }
+
+    public static Priority fromPostName(String postName) throws NoSuchElementException {
+        return Arrays.stream(Priority.values())
+                .filter(priority -> Objects.equals(priority.POST_NAME, postName))
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("No such priority with post name \"" + postName + "\"."));
     }
 }
